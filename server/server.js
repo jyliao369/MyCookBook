a16f97e535bc86ad1ab8d432f59848ab6d5e6a29
jyliao369 <jyliao369@gmail.com>
2021-11-14 11:40:04 -0500

    deploying again

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
-// const PORT = process.env.PORT || 3001;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 mongoose.connect(
