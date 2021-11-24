3c33a5ddd806aaab8077ff5078ca6021b996ab2f
jyliao369 <jyliao369@gmail.com>
2021-11-24 13:17:43 -0500

    making update form

--- client/src/pages/UpdateRecipe.js
+++ client/src/pages/UpdateRecipe.js
@@ -13,6 +13,7 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 
 import { Image } from 'cloudinary-react';
+import { valueFromAST } from 'graphql';
 
 const UpdateRecipe = () => {
 
@@ -22,23 +23,80 @@ const UpdateRecipe = () => {
         variables: { recipeId: recipeId },
     });
 
+    // THESE CODES HERE SHOULD GRAB THE RECIPE AND LOAD THE PRESET DATA
+    // INTO THE RESPECTFUL AREAS OF THE FORM
     const recipe = data?.recipe || {};
-    console.log('recipe');
-    console.log(recipe);
+    // console.log('recipe');
+    // console.log(recipe);
 
     let ingredientList = [''];
-    // for (let a = 0; a < recipe.ingredients.length; a++) {
-    //     ingredientList += (recipe.ingredients[a] + '\r\n');
-    //     console.log(ingredientList);
-    // }
+    if (recipe?.ingredients) {
+        for (let a = 0; a < recipe.ingredients.length; a++) {
+            ingredientList += (recipe.ingredients[a] + '\r\n');
+            // console.log(ingredientList);
+        }
+    }
 
-    console.log(ingredientList);
+    let directionList = [''];
+    if (recipe?.directions) {
+        for (let b = 0; b< recipe.directions.length; b++) {
+            directionList += (recipe.directions[b] + '\r\n');
+            // console.log(directionList);
+        }
+    }
 
-    let directionList = '';
-    // for (let b = 0; b< recipe.directions.length; b++) {
-    //     directionList += (recipe.directions[b] + '\r\n');
-    //     console.log(directionList);
-    // }
+    // THESE NEXT CODES SHOULD, IN MOST BASIC TERMS, USE A USESTATE TO BASICALLY 
+    // CREATE AN UPDATED RECIPE. SHOULD HAVE THE SAME CODE AS THE ADD/CREATE NEW RECIPE
+    const [updateRecipe, setUpdateRecipe] = useState({
+        title: '',
+        category: '',
+        servings: '',
+        totalTime: '',
+        ingredients: [''],
+        directions: [''],
+        imageid: '',
+    });
+
+    console.log('updateRecipe');
+    console.log(updateRecipe);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+
+        if (name === "title") {
+            setUpdateRecipe((prevState) => ({
+
+                ...prevState,
+                title: value,
+            }))
+        }
+
+        if (name === "category") {
+            setUpdateRecipe((prevState) => ({
+
+                ...prevState,
+                category: value,
+            }))
+        }
+
+        if (name === "servings") {
+            setUpdateRecipe((prevState) => ({
+
+                ...prevState,
+                servings: value,
+            }))
+        }
+
+        if (name === "totalTime") {
+            setUpdateRecipe((prevState) => ({
+
+                ...prevState,
+                totalTime: value,
+            }))
+        }
+
+    };
+
     
     if (loading) {
         return <h1>Loading</h1>
@@ -47,44 +105,58 @@ const UpdateRecipe = () => {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center'}}>
 
-            <Grid item xs={6}>
+            <Grid item xs={12} md={6}>
                 <Paper sx={{ p: 3 }}>
-                    <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
-                        <TextField 
-                            label="Recipe Name"
-                            sx={{ p: 2 }}
-                            defaultValue={recipe.title} 
-                        ></TextField>
-                        <TextField 
-                            label="Category"
-                            sx={{ p: 2 }}
-                            defaultValue={recipe.category} 
-                        ></TextField>
-                        <TextField 
-                            label="Servings"
-                            sx={{ p: 2 }}
-                            defaultValue={recipe.servings} 
-                        ></TextField>
-                        <TextField 
-                            label="Total Time"
-                            sx={{ p: 2 }}
-                            defaultValue={recipe.totalTime} 
-                        ></TextField>
+                    <Grid sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                        <Grid sx={{ display: 'flex', flexDirection: 'column', width: 400 }}>
+                            <TextField 
+                                label="Recipe Name"
+                                sx={{ m: 2 }}
+                                defaultValue={recipe.title}
+                                name ="title"
+                                onChange={handleChange} 
+                            ></TextField>
+                            <TextField 
+                                label="Category"
+                                sx={{ m: 2 }}
+                                defaultValue={recipe.category}
+                                name="cateogry"
+                                onChange={handleChange} 
+                            ></TextField>
+                            <TextField 
+                                label="Servings"
+                                sx={{ m: 2 }}
+                                defaultValue={recipe.servings}
+                                name="servings" 
+                                onChange={handleChange}  
+                            ></TextField>
+                            <TextField 
+                                label="Total Time"
+                                sx={{ m: 2 }}
+                                defaultValue={recipe.totalTime}
+                                name="totalTime" 
+                                onChange={handleChange} 
+                            ></TextField>
+                        </Grid>
+                        <Grid sx={{ display: 'flex' }}>
+                            {recipe.imageid ? (
+                                <Image width='100%' cloudName="du119g90a" public_id={recipe.imageid} />
+                            ) : ( 
+                                <Image width='100%' cloudName="du119g90a" public_id="https://res.cloudinary.com/du119g90a/image/upload/v1636841468/noimage_w8jxmo.jpg"/>
+                            )}           
+                        </Grid>
                     </Grid>
-                    <Grid>
-                        <Image></Image>
-                    </Grid>
-                    <Grid sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Grid sx={{ display: 'flex', flexDirection: 'column', mt: 1 }}>
                         <TextField
                             label="Ingredients"
-                            sx={{ p: 2 }}
+                            sx={{ m: 2 }}
                             multiline
                             rows={10}
                             defaultValue={ingredientList}
                         ></TextField>
                         <TextField
                             label="Directions"
-                            sx={{ p: 2 }}
+                            sx={{ m: 2 }}
                             multiline
                             rows={10}
                             defaultValue={directionList}
