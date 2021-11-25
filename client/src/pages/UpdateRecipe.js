8447df4e7b4bd1d19081078829491e226695aeb6
jyliao369 <jyliao369@gmail.com>
2021-11-24 21:05:17 -0500

    work on update form

--- client/src/pages/UpdateRecipe.js
+++ client/src/pages/UpdateRecipe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 import { useParams } from 'react-router-dom';
 
@@ -11,9 +12,10 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
 import { Image } from 'cloudinary-react';
-import { valueFromAST } from 'graphql';
 
 const UpdateRecipe = () => {
 
@@ -32,31 +34,40 @@ const UpdateRecipe = () => {
     let ingredientList = [''];
     if (recipe?.ingredients) {
         for (let a = 0; a < recipe.ingredients.length; a++) {
-            ingredientList += (recipe.ingredients[a] + '\r\n');
-            // console.log(ingredientList);
+            if (a < recipe.ingredients.length-1) {
+                ingredientList += (recipe.ingredients[a] + '\r\n');
+            } else if (a === recipe.ingredients.length - 1){
+                ingredientList += (recipe.ingredients[a]);
+            }
         }
     }
 
     let directionList = [''];
     if (recipe?.directions) {
-        for (let b = 0; b< recipe.directions.length; b++) {
-            directionList += (recipe.directions[b] + '\r\n');
-            // console.log(directionList);
+        for (let b = 0; b < recipe.directions.length; b++) {
+            if (b < recipe.directions.length-1) {
+                directionList += (recipe.directions[b] + '\r\n');
+            } else if (b === recipe.directions.length - 1){
+                directionList += (recipe.directions[b]);
+            }
         }
     }
 
     // THESE NEXT CODES SHOULD, IN MOST BASIC TERMS, USE A USESTATE TO BASICALLY 
     // CREATE AN UPDATED RECIPE. SHOULD HAVE THE SAME CODE AS THE ADD/CREATE NEW RECIPE
+    useEffect(()=>{
+        setUpdateRecipe({...recipe})
+    }, [recipe]);
+    
     const [updateRecipe, setUpdateRecipe] = useState({
         title: recipe.title,
-        category: `${recipe.category}`,
-        servings: `${recipe.servings}`,
-        totalTime: `${recipe.totalTime}`,
+        category: recipe.cateogry,
+        servings: recipe.servings,
+        totalTime: recipe.totalTime,
         ingredients: recipe.ingredients,
         directions: recipe.directions,
-        imageid: `${recipe.imageid}`,
+        imageid: recipe.imageid
     });
-
 
     console.log('updateRecipe');
     console.log(updateRecipe);
@@ -121,6 +132,9 @@ const UpdateRecipe = () => {
         }
     };
 
+    // THESE CODES SHOULD HELP UPLOAD A NEW IMAGE JUST IN CASE USERS WANT TO 
+    // UPLOAD A NEW IMAGE FOR THE RECIPE
+    const [ imageSelected, setImageSelected ] = useState("");
     
     if (loading) {
         return <h1>Loading</h1>
@@ -132,7 +146,7 @@ const UpdateRecipe = () => {
             <Grid item xs={12} md={6}>
                 <Paper sx={{ p: 3 }}>
                     <Grid sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-                        <Grid sx={{ display: 'flex', flexDirection: 'column', width: 400 }}>
+                        <Grid sx={{ display: 'flex', flexDirection: 'column', width: 500 }}>
                             <TextField 
                                 label="Recipe Name"
                                 sx={{ m: 2 }}
@@ -140,13 +154,20 @@ const UpdateRecipe = () => {
                                 name ="title"
                                 onChange={handleChange} 
                             ></TextField>
-                            <TextField 
-                                label="Category"
+                            <Select
+                                // label="Category"
                                 sx={{ m: 2 }}
-                                defaultValue={recipe.category}
+                                variant="outlined"
+                                defaultValue={`${recipe.category}`}
                                 name="category"
-                                onChange={handleChange} 
-                            ></TextField>
+                                onChange={handleChange}
+                                placeholder="Category"
+                            >
+                                <MenuItem value="Drinks">Drinks</MenuItem>
+                                <MenuItem value="Appetizer">Appetizer</MenuItem>
+                                <MenuItem value="Entree">Entree</MenuItem>
+                                <MenuItem value="Dessert">Dessert</MenuItem>
+                            </Select>
                             <TextField 
                                 label="Servings"
                                 sx={{ m: 2 }}
@@ -162,12 +183,35 @@ const UpdateRecipe = () => {
                                 onChange={handleChange} 
                             ></TextField>
                         </Grid>
-                        <Grid sx={{ display: 'flex' }}>
-                            {recipe.imageid ? (
-                                <Image width='100%' cloudName="du119g90a" public_id={recipe.imageid} />
-                            ) : ( 
-                                <Image width='100%' cloudName="du119g90a" public_id="https://res.cloudinary.com/du119g90a/image/upload/v1636841468/noimage_w8jxmo.jpg"/>
-                            )}           
+                        <Grid sx={{ display: 'flex', flexDirection: 'column', m: 2, width: 300 }}>
+                            <Grid>
+                                
+                                {/* {recipe.imageid ? (
+                                    <Image width='100%' cloudName="du119g90a" public_id={recipe.imageid}/>
+                                ) : ( 
+                                    <Image width='100%' cloudName="du119g90a" public_id="https://res.cloudinary.com/du119g90a/image/upload/v1636841468/noimage_w8jxmo.jpg"/>
+                                )} */}
+
+                                {/* THE LOGIC IS THAT IF THERE IS A PREEXISTING IMAGE, SHOW THE IMAGE.
+                                IF THERE IS NO IMAGE, CHECK TO SEE IF THERE IS A NEW IMAGE ADDED.
+                                IF NO NEW IMAGE IS SUPPLIED, 'NO IMAGE' IS RENDERED */}
+                                { (recipe.imageid) ? (
+                                    <Image width='100%' cloudName="du119g90a" public_id={recipe.imageid}/>
+                                ) : ( (imageSelected) ? (
+                                    <img src={URL.createObjectURL(imageSelected)} className="imagepreview" alt=""></img>
+                                ) : (
+                                    <h1>No Image</h1>
+                                ))} 
+                            </Grid>
+                            <Grid>
+                                <input 
+                                    title=" "
+                                    type="file"
+                                    onChange={(event) => {
+                                        setImageSelected(event.target.files[0]);
+                                    }}
+                                />  
+                            </Grid>
                         </Grid>
                     </Grid>
                     <Grid sx={{ display: 'flex', flexDirection: 'column', mt: 1 }}>
@@ -192,8 +236,6 @@ const UpdateRecipe = () => {
                     </Grid>
                 </Paper>
             </Grid>
-
-            {/* <input defaultValue={`${recipe.title}`}></input> */}
 
         </Box>
     );
